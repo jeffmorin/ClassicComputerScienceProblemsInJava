666a762e3af9d9f28924de765a69f1d792bd7cc3
Jean-François Morin <ioannes.franciscus@gmail.com>
2020-07-21 21:56:51 -0400

    Proofed chapter 4

--- CCSPiJ/src/chapter4/UnweightedGraph.java
+++ CCSPiJ/src/chapter4/UnweightedGraph.java
@@ -21,7 +21,7 @@ import java.util.List;
 import chapter2.GenericSearch;
 import chapter2.GenericSearch.Node;
 
-public class UnweightedGraph<V> extends Graph<V, Edge> {
+public class UnweightedGraph<V> extends Graph<V, Edge> implements Cities {
 
 	public UnweightedGraph(List<V> vertices) {
 		super(vertices);
@@ -47,40 +47,15 @@ public class UnweightedGraph<V> extends Graph<V, Edge> {
 	// Test basic Graph construction
 	public static void main(String[] args) {
 		// Represents the 15 largest MSAs in the United States
-		UnweightedGraph<String> cityGraph = new UnweightedGraph<>(
-				List.of("Seattle", "San Francisco", "Los Angeles", "Riverside", "Phoenix", "Chicago", "Boston",
-						"New York", "Atlanta", "Miami", "Dallas", "Houston", "Detroit", "Philadelphia", "Washington"));
+		UnweightedGraph<String> cityGraph = new UnweightedGraph<>(CITIES);
 
-		cityGraph.addEdge("Seattle", "Chicago");
-		cityGraph.addEdge("Seattle", "San Francisco");
-		cityGraph.addEdge("San Francisco", "Riverside");
-		cityGraph.addEdge("San Francisco", "Los Angeles");
-		cityGraph.addEdge("Los Angeles", "Riverside");
-		cityGraph.addEdge("Los Angeles", "Phoenix");
-		cityGraph.addEdge("Riverside", "Phoenix");
-		cityGraph.addEdge("Riverside", "Chicago");
-		cityGraph.addEdge("Phoenix", "Dallas");
-		cityGraph.addEdge("Phoenix", "Houston");
-		cityGraph.addEdge("Dallas", "Chicago");
-		cityGraph.addEdge("Dallas", "Atlanta");
-		cityGraph.addEdge("Dallas", "Houston");
-		cityGraph.addEdge("Houston", "Atlanta");
-		cityGraph.addEdge("Houston", "Miami");
-		cityGraph.addEdge("Atlanta", "Chicago");
-		cityGraph.addEdge("Atlanta", "Washington");
-		cityGraph.addEdge("Atlanta", "Miami");
-		cityGraph.addEdge("Miami", "Washington");
-		cityGraph.addEdge("Chicago", "Detroit");
-		cityGraph.addEdge("Detroit", "Boston");
-		cityGraph.addEdge("Detroit", "Washington");
-		cityGraph.addEdge("Detroit", "New York");
-		cityGraph.addEdge("Boston", "New York");
-		cityGraph.addEdge("New York", "Philadelphia");
-		cityGraph.addEdge("Philadelphia", "Washington");
-		System.out.println(cityGraph.toString());
+		for (Route route : ROUTES) {
+			cityGraph.addEdge(route.city1, route.city2);
+		}
+		System.out.println(cityGraph);
 
-		Node<String> bfsResult = GenericSearch.bfs("Boston",
-				v -> v.equals("Miami"),
+		Node<String> bfsResult = GenericSearch.bfs(BOSTON,
+				v -> v.equals(MIAMI),
 				cityGraph::neighborsOf);
 		if (bfsResult == null) {
 			System.out.println("No solution found using breadth-first search!");
